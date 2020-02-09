f620d071441f10d75d0a4e9a1de576079d15f828
zakriev <57766523+zakriev@users.noreply.github.com>
2020-02-09 22:29:20 +0100

    Add files via upload

---
+++ hussein-zakrijev-js1-ma3.js
@@ -0,0 +1,70 @@
+// question 1
+const arrow = (a, b) => a - b;
+
+// question 2
+
+fetch ("https://api.rawg.io/api/games?genres=sports%22)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (json) {
+        for (let i = 0; i < json.results.length; i++) {
+            console.log(json.results[i].name);
+        }
+    })
+
+    .catch(() => document.location.href = "error.html");
+
+
+// question 3
+let replaceText = "These cats are outrageous.";
+replaceText = replaceText.replace("cats", "giraffes");
+
+// question 4 
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+
+let userID;
+
+if (params.has("userId")) {
+    userID = params.get("userId");
+}
+
+if(userID < 10){
+    document.location.href = "first.html"
+} else if (userID >= 10){
+    document.location.href = "second.html";
+} else{
+    document.location.href = "third.html";
+}
+
+// question 5
+const container = document.querySelector(".container");
+const button = document.querySelector(".btn");
+container.removeChild(button);
+
+// question 6
+
+const animals = document.querySelector(".animals");
+const cows = document.querySelector(".cows");
+
+const listing = document.createElement("li");
+listing.className = "parrots"
+listing.innerText = "Parrots";
+
+cows.after(listing);
+
+// question 7
+
+fetch("https://api.rawg.io/api/games/3801%22)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (json) {
+        const rating = document.querySelector(".rating");
+        rating.appendChild(document.createTextNode(json.rating));
+    })
+
+    .catch(function (error) {
+        console.log(error);
+    });
